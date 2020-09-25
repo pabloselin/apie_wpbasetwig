4fce292c583b4dd5e41f8258642906f525c9651f
Pablo Selín Carrasco Armijo <pablo@apie.cl>
2020-09-25 10:30:51 -0300

    ajustes varios tema

--- src/js/bundle.js
+++ src/js/bundle.js
@@ -4,4 +4,23 @@
 //import "./extras/navigation";
 //import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.js"
 
+function ready(fn) {
+	if (document.readyState != "loading") {
+		fn();
+	} else {
+		document.addEventListener("DOMContentLoaded", fn);
+	}
+}
+
+const back = () => {
+	const backButton = document.getElementById("backToSearch");
+	if (backButton) {
+		backButton.addEventListener("click", () => {
+			history.back();
+		});
+	}
+};
+
+ready(back);
+
 console.log("bundle");
