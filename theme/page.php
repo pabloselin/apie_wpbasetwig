f83b238e7845f9b202c92636dd35d4af1f02d322
Pablo Selín Carrasco Armijo <pablo@apie.cl>
2020-10-16 11:23:15 -0300

    ajustes moviles y home nuevo

--- theme/page.php
+++ theme/page.php
@@ -25,4 +25,11 @@ $context = Timber::context();
 
 $timber_post     = new Timber\Post();
 $context['post'] = $timber_post;
-Timber::render( array( 'page-' . $timber_post->post_name . '.twig', 'page.twig' ), $context );
+
+if($timber_post->ID == get_option('page_on_front')) {
+	$template = 'home.twig';
+} else {
+	$template = array( 'page-' . $timber_post->post_name . '.twig', 'page.twig' );
+}
+
+Timber::render( $template, $context );
